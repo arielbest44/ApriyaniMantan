0892f54ea5fb7483007001d70f0af532827cec63
arielbest44 <56075835+arielbest44@users.noreply.github.com>
2019-10-07 20:00:49 +0700

    Add files via upload

---
+++ Triggred.sh
@@ -0,0 +1,99 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay TOOLS
+toilet -f big -F gay TRIGGRED
+echo $green"=====================================
+AUTHOR : ARIEL DAN APRIYANI
+YOUTUBE : AG OFFICIAL
+===================================="
+toilet -f big -F gay Maaf
+toilet -f big -F gay Tools
+toilet -f big -F gay Cuma 3
+echo
+sleep 2
+echo $yellow"Silahkan Pilih Nomer Berapa :"
+echo
+echo $yellow"Nomer 2 Username Nya ridwan
+password nya erinafahira"
+echo
+echo $cyan"1.) LITEDDOS"
+echo
+echo $red"2.) PIPHISH"
+echo
+echo $green"3.) SQU4D"
+echo
+echo $green"4.) Fb Target"
+echo $blue
+read -p "Ariel Ganteng Kan?
+pilih Nomer Berapa Lu? : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Ariel
+echo
+toilet -f big -F gay 69
+echo $green"Ariel Sayang Apriyani"
+sleep 2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay I Love
+toilet -f big -F gay You
+toilet -f big -F gay Apriyani
+echo
+echo $green"I Love You Apriyani"
+sleep 1
+git clone https://github.com/ridwanirawan/PiPhish
+cd PiPhish
+sh run.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f big -F gay Baruna
+toilet -f big -F gay 1928
+toilet -f big -F gay SQU4D
+toilet -f big -F gay 1928
+echo $yellow"BUCIN DULU EAAAAAAAAAA"
+sleep 1
+toilet -f big -F gay GUA
+toilet -f big -F gay Sayang
+toilet -f big -F gay Apriyani
+toilet -f big -F gay Ariel
+toilet -f big -F gay Gaming
+toilet -f big -F gay Official
+fi
+
+fi [ $bro = 4 ] || [ $bro = 4
+then
+clear
+toilet -f big -F gay Ariel 69
+echo
+echo $green"Bacot"
+sleep 1
+git clone https://github.com/sixtysix-Team/fbbrute
+cd fbbrute
+ls
+python2 force.py
+fi
+
+if [ $bro = 0 ] || [$bro = 0 ]
+then
+clear
+exit
+if
